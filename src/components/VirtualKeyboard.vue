03a339aa28ec488803ec8f0b98dd834752f7b72a
Khôi Tran <tran@puzzle.ch>
2017-12-04 08:36:05 +0100

    gh-pages

--- src/components/VirtualKeyboard.vue
+++ src/components/VirtualKeyboard.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
     <h1>Virtual Keyboard</h1>
-    <ul>
-
-    </ul>
+    <div>
+      <div class="octave">
+        <div class="C">C</div>
+        <div class="Db">Db</div>
+        <div class="D">D</div>
+        <div class="E">E</div>
+        <div class="Eb">Eb</div>
+        <div class="F">F</div>
+        <div class="Gb">Gb</div>
+        <div class="G">G</div>
+        <div class="Ab">Ab</div>
+        <div class="A">A</div>
+        <div class="Bb">Bb</div>
+        <div class="B">B</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+// ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
 export default {
   name: 'VirtualKeyboard',
   mounted () {
